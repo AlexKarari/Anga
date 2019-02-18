1f0b8af1540c216e012b85d1987fe323bf32d3c6
AlexKarari <karari.alexander@gmail.com>
2019-02-18 14:46:06 +0300

    API Passed
    API KEY and requests for hard coded city data currently working

--- angaApp/urls.py
+++ angaApp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    
+    url('^$', views.index, name='landingpage'), #Path to the index view
 ]
--- angaApp/views.py
+++ angaApp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=0670d657e3a550164aee16950a8e1114'
+    city = 'Las Vegas'
+    city_weather = requests.get(url.format(city)).json() #Requesting the API data and converting the JSON into Python objects
+    weather = {
+
+        'city': city,
+        'temperature': city_weather['main']['temp'],
+        'description': city_weather['weather'][0]['description'],
+        'icon': city_weather['weather'][0]['icon']
+    }
+
+    context = {'weather':weather}
+
+    return render(request, 'index.html', context) #Returns the index.html template
+
